768d357c8b08379e94b1b547accdb3b47c4a3a49
Mark <mab688@pitt.edu>
2024-05-30 15:19:38 -0400

    Import global CSS file in layout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { Stack } from "expo-router";
+// Import your global CSS file
+import "../global.css"
 
 export default function RootLayout() {
   return (
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
 import { Text, View } from "react-native";
-import "../global.css"
 
 export default function Index() {
   return (
