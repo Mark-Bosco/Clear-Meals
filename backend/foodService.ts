7178d8e416ea6dbb6bbb894895fd260f0d3dcde5
Mark <mab688@pitt.edu>
2024-06-16 22:33:55 -0400

    Working on API

---
+++ backend/foodService.ts
@@ -0,0 +1,3 @@
+/*
+import requestFatSecretApi from './apiClient';
+*/
--- backend/oauth.ts
+++ backend/oauth.ts
@@ -1,26 +1,5 @@
+// oauth.ts
 import OAuth from 'oauth-1.0a';
-import CryptoJS from 'crypto-js';
+import * as CryptoJS from 'crypto-js';
+import { consumerKey, consumerSecret } from './config';
 
-const consumerKey = '';
-const consumerSecret = '';
-
-const oauth = new OAuth({
-  consumer: { key: consumerKey, secret: consumerSecret },
-  signature_method: 'HMAC-SHA1',
-  hash_function(base_string, key) {
-    return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
-  },
-});
-
-export const getOAuthHeader = (url: string, method: 'GET' | 'POST') => {
-  const requestData = {
-    url,
-    method,
-  };
-
-  const oauthData = oauth.authorize(requestData);
-  return {
-    ...oauth.toHeader(oauthData),
-    'Content-Type': 'application/json',
-  };
-};
